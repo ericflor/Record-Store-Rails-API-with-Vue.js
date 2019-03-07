95e4002383a9aee94c79c076fd71aaa2eb7b2073
Eric Florence <ericflorence.dev@gmail.com>
2019-03-06 16:59:55 -0800

    add all the components, update routes, fix the bugz

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,5 +1,4 @@
 class SignupController < ApplicationController
-  
   def create
     user = User.new(user_params)
 
@@ -8,22 +7,21 @@ class SignupController < ApplicationController
       session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
       tokens = session.login
 
-      response.set_cookie(JWTSessions.access_cookie, 
+      response.set_cookie(JWTSessions.access_cookie,
                           value: tokens[:access],
                           httponly: true,
                           secure: Rails.env.production? )
-      render json: { csrf: tokens[:csrf]}
+
+      render json: { csrf: tokens[:csrf] }
     else
       render json: { error: user.errors.full_messages.join(' ') }, status: :unprocessable_entity
-
     end
   end
 
-  private 
+
+  private
 
     def user_params
       params.permit(:email, :password, :password_confirmation)
     end
-  
-
 end
